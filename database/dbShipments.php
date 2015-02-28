ed793307227142062022c904673fe4c3dff1bf1c
martinda <martinda@whitman.edu>
2015-02-28 13:10:48 -0800

    Updated dbShipments and shipmentSearch to account for the date range.

--- database/dbShipments.php
+++ database/dbShipments.php
@@ -60,12 +60,13 @@ function getall_dbShipments(){
 	return $theVols;
 }
 
-
-function getonlythose_dbShipments($customer_id, $ship_date, $ship_items) {
+//Edited this function to make it specific to the search queries
+function getonlythose_dbShipments($customer_id, $ship_date1, $ship_date2, $ship_items) {
 	connect();
-	$query = "SELECT * FROM dbShipments WHERE customer_id LIKE '%".$customer_id."%'" .
-			 " AND ship_date LIKE '%".$ship_date."%'" . 
-			 " AND ship_items LIKE '%".$ship_items."%'" ;
+	$query = "SELECT * FROM dbShipments WHERE customer_id LIKE '%".$customer_id."%'";
+	if(empty($ship_date1)==false) $query .= " AND ship_date >= '%".$ship_date1."%'";		
+	if(empty($ship_date2)==false) $query .= " AND ship_date >= '%".$ship_date2."%'";
+	$query .= " AND ship_items LIKE '%".$ship_items."%'" ;
 	$query .= " ORDER BY customer_id";
 	$result = mysql_query($query);
 	$theShipments = array();
