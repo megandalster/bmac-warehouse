559ff0fdb1b999766da0196c77d88919ed635625
elbadrmm <elbadrmm@whitman.edu>
2015-03-14 02:23:25 -0700

    customerEdit & customerForm

--- customerEdit.php
+++ customerEdit.php
@@ -1,33 +1,32 @@
 <?PHP
 /*
- * Copyright 2014 by Allen Tucker. 
- * This program is part of BMAC-Warehouse, which is free software.
+ * Copyright 2014 by Moustafa El Badry, Noah Jensen, Dylan Martin, Luis Munguia Orta,
+ * David Quennoz, and Allen Tucker. This program is part of BMAC-Warehouse, which is free software.
  * It comes with absolutely no warranty.  You can redistribute and/or
  * modify it under the terms of the GNU Public License as published
  * by the Free Software Foundation (see <http://www.gnu.org/licenses/).
 */
 
 /*
- *	personEdit.php
- *  oversees the editing of a person to be added, changed, or deleted from the database
- *	@author Allen Tucker
- *	@version December 29, 2014
+ *	customerEdit.php
+ *  oversees the editing of a customer to be added, changed, or deleted from the database
+ *	@author Moustafa ElBadry
+ *	@version March 04, 2015
  */
 	session_start();
 	session_cache_expire(30);
-    include_once('database/dbPersons.php');
-    include_once('domain/Person.php'); 
+    include_once('database/dbCustomers.php');
+    include_once('domain/Customer.php'); 
     
 //    include_once('database/dbLog.php');
-	$id = $_GET["id"];
-	if ($id=='new') {
-	 	$person = new Person(null,'new',null,null,null,null,null,null,null,null,null,
-	 	     	null,md5("new"));
+	$customer_id = $_GET["id"];
+	if ($customer_id=='new') {
+	 	$customer = new Customer('new',null,null,null,null,null,null,null,null,null,null,null);
 	}
 	else {
-		$person = retrieve_dbPersons($id);
-		if (!$person) {
-	         echo('<p id="error">Error: there\'s no person with this id in the database</p>'. $id);
+		$customer = retrieve_dbCustomers($customer_id);
+		if (!$customer) {
+	         echo('<p customer_id="error">Error: there\'s no customer with this customer_id in the database</p>'. $customer_id);
 		     die();
         }  
 	}
@@ -35,7 +34,7 @@
 <html>
 	<head>
 		<title>
-			Editing <?PHP echo($person->get_first_name()." ".$person->get_last_name());?>
+			Editing <?PHP echo($customer->get_customer_id()." ".$customer->get_phone());?>
 		</title>
 		<link rel="stylesheet" href="styles.css" type="text/css" />
 	</head>
@@ -46,36 +45,35 @@
 <?PHP
 	if($_POST['_form_submit']!=1){
 	//in this case, the form has not been submitted, so show it
-			include('personForm.inc');
+			include('customerForm.inc');
 	}
 	else {
 	//in this case, the form has been submitted, so validate it
-		if ($_POST['availability']==null)
-			   $avail = "";
-		else $avail = implode(',',$_POST['availability']);
-		if ($id=='new') {
-				$first_name = trim($_POST['first_name']);
-				$last_name = $_POST['last_name'];
-				$phone1 = $_POST['phone1'];
+		
+		if ($customer_id=='new') {
+				$customer_id = trim($_POST['customer_id']);
+				$phone = $_POST['phone'];
+				$email = $_POST['email'];
 		}
 		else {
-				$first_name = $person->get_first_name();
-				$last_name = $person->get_last_name();
-				$phone1 = $person->get_phone1();
+				$customer_id = $customer->get_customer_id();
+				$phone = $customer->get_phone();
+				$email = $customer->get_email();
 		}
-		$person = new Person($last_name, $first_name, $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip'],
-								 $phone1, $_POST['phone2'], $_POST['email'], $_POST['type'],
-								 $_POST['status'], $_POST['notes'], $_POST['old_pass']);
-		$errors = validate_form($id); 	//step one is validation.
+		$customer = new Customer($customer_id, $_POST['code'], $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip'],
+								 $_POST['county'], $_POST['contact'],$_POST['phone'],
+								 $_POST['email'], $_POST['status'], $_POST['notes']);
+								 
+		$errors = validate_form($customer_id); 	//step one is validation.
         // errors array lists problems on the form submitted
 		if ($errors) {
 		// display the errors and the form to fix
 			show_errors($errors);
-			include('personForm.inc');
+			include('customerForm.inc');
 		}
 		// this was a successful form submission; update the database and exit
 		else
-			process_form($id, $person);
+			process_form($customer_id, $customer);
 		include('footer.inc');
 		echo('</div></div></body></html>');
 		die();
@@ -84,87 +82,74 @@
 /**
 * process_form sanitizes data, concatenates needed data, and enters it all into a database
 */
-function process_form($id, $person)	{
+function process_form($customer_id, $customer)	{
 	//step one: sanitize data by replacing HTML entities and escaping the ' character
-		if ($id=='new') {
-			    $first_name = trim(str_replace('\\\'','',htmlentities(trim($_POST['first_name']))));
-				$last_name = trim(str_replace('\\\'','\'',htmlentities($_POST['last_name'])));
-				$phone1 = trim(str_replace(' ','',htmlentities($_POST['phone1'])));
-				$clean_phone1 = preg_replace("/[^0-9]/", "", $phone1);
+		if ($customer_id=='new') {
+			    $customer_id = trim(str_replace('\\\'','',htmlentities(trim($_POST['customer_id']))));
+				$code = trim(str_replace('\\\'','\'',htmlentities($_POST['code'])));
+				$address = trim(str_replace(' ','',htmlentities($_POST['address'])));
+				$clean_phone = preg_replace("/[^0-9]/", "", $phone);
 		}
 		else {
-				$first_name = $person->get_first_name();
-				$last_name = $person->get_last_name();
-				$phone1 = $person->get_phone1();
-				$clean_phone1 = $phone1;
+				$customer_id = $customer->get_customer_id();
+				$code = $customer->get_code();
+				$phone = $customer->get_phone();
+				$clean_phone = $phone;
 		}
 		$address = trim(str_replace('\\\'','\'',htmlentities($_POST['address'])));
 		$city = trim(str_replace('\\\'','\'',htmlentities($_POST['city'])));
 		$state = trim(htmlentities($_POST['state']));
 		$zip = trim(htmlentities($_POST['zip']));
-		$phone2 = trim(str_replace(' ','',htmlentities($_POST['phone2'])));
-		$clean_phone2 = preg_replace("/[^0-9]/", "", $phone2);
-		$email = $_POST['email'];
-		$type = $_POST['type'];			
-        $status = $_POST['status'];
-        $notes = trim(str_replace('\\\'','\'',htmlentities($_POST['my_notes'])));
-		$pass = $_POST['password'];
+		$county = trim(htmlentities($_POST['county']));
+		$contact = trim(str_replace(' ','',htmlentities($_POST['contact'])));
+		$clean_contact = preg_replace("/[^0-9]/", "", $contact);
+	    $email = $_POST['email'];
+	    $status = $_POST['status'];
+        $notes =  trim(str_replace('\\\'','\'',htmlentities($_POST['notes'])));
 		
-		$newperson = new Person($last_name, $first_name, $address, $city, $state, $zip, $clean_phone1, 
-						$clean_phone2, $email, $type, $status, $notes, $pass);
+		
+		$newcustomer = new Customer($customer_id, $code, $address, $city, $state, $zip, 
+						$county, $contact, $phone, $email, $status, $notes);
         
 	//step two: try to make the deletion, addition, or change
 		if($_POST['deleteMe']=="DELETE"){
-			$result = retrieve_dbPersons($id);
+			$result = retrieve_dbCustomers($customer_id);
 			if (!$result)
-				echo('<p>Unable to delete. ' .$first_name.' '.$last_name. ' is not in the database. <br>Please report this error to the House Manager.');
+				echo('<p>Unable to delete. ' .$customer_id. ' is not in the database. <br>Please report this error to the House Manager.');
 			else {
-				//What if they're the last remaining manager account?
-				if($type=='manager'){
-				//They're another manager, we need to check that they can be deleted
-					$managers = getonlythose_dbPersons('manager','','');
-					if ($id==$_SESSION['_id'] || !$managers || sizeof($managers) <= 1)
-						echo('<p class="error">You cannot remove yourself or the last remaining manager from the database.</p>');
-					else {
-						$result = delete_dbPersons($id);
-						echo("<p>You have successfully removed " .$first_name." ".$last_name. " from the database.</p>");
-					}
+					$result = delete_dbCustomers($customer_id);
+					echo("<p>You have successfully removed " .$customer_id. " from the database.</p>");		
 				}
-				else {
-					$result = delete_dbPersons($id);
-					echo("<p>You have successfully removed " .$first_name." ".$last_name. " from the database.</p>");		
-				}
-			}
 		}
 
-		// try to add a new person to the database
+		// try to add a new customer to the database
 		else if ($_POST['old_id']=='new') {
-			    $id = $first_name.$clean_phone1;
+			    $customer_id = get_customer_id();
 				//check if there's already an entry
-				$dup = retrieve_dbPersons($id);
+				$dup = retrieve_dbCustomers($customer_id);
 				if ($dup)
-					echo('<p class="error">Unable to add ' .$first_name.' '.$last_name. ' to the database. <br>Another person with the same id is already there.');
+					echo('<p class="error">Unable to add ' .$customer_id.  ' to the database. <br>Another customer with the same id is already there.');
 				else {
-					$newperson->set_password (md5($first_name.$clean_phone1));
-					$result = insert_dbPersons($newperson);
+					    $result = insert_dbCustomers($newcustomer);
 					if (!$result)
-                        echo ('<p class="error">Unable to add "' .$first_name.' '.$last_name. '" to the database. <br>Please report this error to the Program manager.');
-					else echo("<p>You have successfully added " .$first_name." ".$last_name. " to the database.</p>");
+                        echo ('<p class="error">Unable to add "' .$customer_id.' " to the database. <br>Please report this error to the Program manager.');
+					else echo("<p>You have successfully added <a href='customerEdit.php?id=" .$customer_id.  "'>"
+					              .$customer_id."</a> to the database.</p>");
 				}
 		}
 
-		// try to replace an existing person in the database by removing and adding
+		// try to replace an existing customer in the database by removing and adding
 		else {
-				$id = $_POST['old_id'];
-				$result = delete_dbPersons($id);
+				$result = delete_dbCustomers($customer_id);
                 if (!$result)
-                   echo ('<p class="error">Unable to update ' .$first_name.' '.$last_name. '. <br>Please report this error to the Program manager.');
+                   echo ('<p class="error">Unable to update ' .$customer_id. '. <br>Please report this error to the Program manager.');
 				else {
-					$result = insert_dbPersons($newperson);
+					$result = insert_dbCustomers($newcustomer);
                 	if (!$result)
-                   		echo ('<p class="error">Unable to update ' .$first_name.' '.$last_name. '. <br>Please report this error to the Foodbank Director.');
-					else echo("<p>You have successfully updated " .$first_name." ".$last_name. " in the database.</p>");
-//					add_log_entry('<a href=\"viewPerson.php?id='.$id.'\">'.$first_name.' '.$last_name.'</a>\'s database entry has been updated.');
+                   		echo ('<p class="error">Unable to update ' .$customer_id.  '. <br>Please report this error to the Foodbank Director.');
+					else echo("<p>You have successfully updated <a href='customerEdit.php?id=" .$customer_id.  "'>"
+					              .$customer_id."</a> in the database.</p>");
+//					add_log_entry('<a href=\"viewCustomer.php?id='.$id.'\">'.$customer_id.' '.$phone.'</a>\'s database entry has been updated.');
 				}
 		}
 }
@@ -172,18 +157,15 @@ function process_form($id, $person)	{
  * 
  * 
  */
-function validate_form($id){
-	if($id=='new' && ($_POST['first_name']==null || $_POST['first_name']=='new')) $errors[] = 'Please enter a first name';
-	if($id=='new' && $_POST['last_name']==null) $errors[] = 'Please enter a last name';
-	if($id=='new' && !valid_phone($_POST['phone1'])) $errors[] = 'Please enter a valid primary phone number (10 digits: ###-###-####)';
+function validate_form($customer_id){
+	if($customer_id=='new' && ($_POST['customer_id']==null || $_POST['customer_id']=='new')) $errors[] = 'Please enter a Customer ID';
+	if($customer_id=='new' && $_POST['code']==null) $errors[] = 'Please enter a Customer code';
+	if($customer_id=='new' && !valid_phone($_POST['phone'])) $errors[] = 'Please enter a valid primary phone number (10 digits: ###-###-####)';
 	if($_POST['city']==null) $errors[] = 'Please enter a city';
 	if($_POST['address']==null) $errors[] = 'Please enter an address';
 	if($_POST['state']==null) $errors[] = 'Please enter a state';
-	if(($_POST['zip'] != strval(intval($_POST['zip']))) || ($_POST['zip']==null) || (strlen($_POST['zip'])!=5)) $errors[] = 'Please enter a valid zip code';
-	if($_POST['type']==null && $_SESSION['access_level']>=1) $errors[] = 'Please select a Type';
-	if ($_SESSION['access_level']<3 && $_POST['type']=='manager')
-		$errors[] = "Sorry, you can't promote yourself to a mamager.";
-	if($_POST['phone2']!=null && !valid_phone($_POST['phone2'])) $errors[] = 'Please enter a valid secondary phone number (10 digits: ###-###-####)';
+	if(($_POST['zip'] != strval(intval($_POST['zip']))) || ($_POST['zip']==null) || (strlen($_POST['zip'])<5)) $errors[] = 'Please enter a valid zip code';
+	if($_POST['phone']!=null && !valid_phone($_POST['phone'])) $errors[] = 'Please enter a valid phone number (10 digits: ###-###-####)';
 	if(!valid_email($_POST['email']) && $_POST['email']!=null) $errors[] = "Please enter a valid email";
 	return $errors;
 }
