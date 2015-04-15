77bf9ba58b04a57b69e969770c4594d7dce9a048
allen <allen@bowdoin.edu>
2015-04-14 19:02:18 -0700

    more progress with inventory worksheet

--- inventory.php
+++ inventory.php
@@ -11,44 +11,76 @@
  * @author Luis Munguia
  * @version 3/13/2015
  */
+	session_start();
+	session_cache_expire(30);
+	
 include_once('database/dbContributions.php');
 include_once('domain/Contribution.php');
 include_once('database/dbShipments.php');
 include_once('domain/Shipment.php'); 
 include_once('database/dbProducts.php');
 include_once('domain/Product.php'); 
-     echo ("Current Inventory Worksheet<br> Worksheet date: ".date('F d, Y')."<br><br>");
+?>
+<html>
+	<head>
+		<title>
+			BMAC-Warehouse Inventory script
+		</title>
+		<link rel="stylesheet" href="styles.css" type="text/css" />
+		<link rel="stylesheet" href="lib/jquery-ui.css" />
+		<script src="lib/jquery-1.9.1.js"></script>
+		<script src="lib/jquery-ui.js"></script>
+		<script>
+		$(function() {
+			    $( "#target" ).scroll();
+		});
+		</script>
+	</head>
+	<body>
+		<div id="container">
+			<?PHP include('header.php');?>
+			<div id="content">
+<?php
+     echo "<p><b>Current Inventory Worksheet</b><br> Today's date: ".date('F d, Y')."<br><br>";
+	 echo '<form method ="post"><div id="target" style="overflow: scroll; width: variable; height: 300px;">';
 	 echo "<table><tr><td colspan=3> </td>".
 	      "<td colspan=3>Last Inventory</td><td colspan=2>Shipments</td><td colspan=2>Receipts</td><td colspan=2>Current Stock</td></tr>";
 	 echo "<tr><td>Product</td><td>Funds Source</td><td>Unit Wt</td><td>Date</td><td>Case Lots</td><td>Total Wt</td>".
-	      "<td>Number</td><td>Total Wt</td<td>Number</td><td>Total Wt</td>>".
+	      "<td>Number</td><td>Total Wt</td><td>Number</td><td>Total Wt</td>".
 	      "<td>Case Lots</td><td>Total Wt</td></tr>";
-	 $products = getall_dbProducts();
-	 echo '<form method ="post"><div id="target" style = flow: scroll; width: variable; height: 300px;">';
+	 $products = getonlythose_dbProducts("","","active");
 	 foreach ($products as $product){
-	 	if ($product->get_history()) 
-	 		$last_inventory = end($product->get_history);
+	 	echo "<tr>";
+	 	if (count($product->get_history())>0) 
+	 		$last_inventory = explode(":",end($product->get_history()));  // the array [date,caselots,weight]
 	 	else 
-	 		$last_inventory = "::" ;
-	 	$ship_items = count_shipments($product->get_id(), $product->get_funding_source());
-	 	$receive_items = count_receipts($product->get_id(), $product->get_funding_source());
-	 	$initial_stock = reset($product_get_history());
-	 	if ($last_inventory=="::"){
-	 		$initial_stock_array = explode(":",$initial_stock);
-	 		$receive_items_array = explode(":",$receive_items);
-	 		$ship_items_array = explode(":",$ship_items);
-	 		$estimated_stock = $initial_stock_array[1] + $receive_items_array[1] - $ship_items_array[1].":"."WEIGHT";
-	 	}
-	 	else $current_stock = $last_inventory;
+	 		$last_inventory = explode(":","::") ;
+	 	$ship_items = explode(":",count_shipments($product->get_product_id(), $product->get_funding_source(),"",""));   // the array [number,weight]
+	 	$receive_items = explode(":",count_receipts($product->get_product_id(), $product->get_funding_source(),"","")); // the array [number,weight]
+	 	if ($last_inventory[0]=="")
+	 		$estimated_stock = "";
+	 	else $estimated_stock = $last_inventory[2] + $receive_items[1] - $ship_items[1];
 	 	// display a line in the table
-	 	echo "<br>".$product->get_id() .
-	 		 $product ->get_funding_source().
-	 		 $product->get_unit_weight .
-	 		 $last_inventory .
-	 		 $ship_items .
-	 		 $receive_items  .
-	 		 '<input type = "text" name="current_case_lots" value ="'.'">' .
-	 		 '<input type = "text" name="current_weight" value ="'.'">';
+	 	echo "<td>".$product->get_product_id() . "</td>".
+	 		 "<td>".$product->get_funding_source(). "</td>".
+	 		 "<td>".$product->get_unit_weight(). "</td>".
+	 		 "<td>".pretty_date($last_inventory[0]) ."</td><td>".$last_inventory[1] ."</td><td>".$last_inventory[2] ."</td>".
+	 		 "<td>".$ship_items[0] ."</td><td>".$ship_items[1] ."</td>".
+	 		 "<td>".$receive_items[0]  ."</td><td>".$receive_items[1] ."</td>".
+	 		 "<td>".'<input type = "text" name="current_case_lots" value ="'.'">' ."</td>".
+	 		 "<td>".'<input type = "text" name="current_weight" value ="'.$estimated_stock.'"></td>';
+	    echo "</tr>";
 	 }
-	 echo '</div> </form>';
-	?>
+	 echo '</table></div></form>';
+	 
+	 function pretty_date($d) {
+	 	if ($d=="")
+	 	    return "";
+	 	return date('m/d/y',mktime(0,0,0,substr($d,3,2),substr($d,6,2),substr($d,0,2)));
+	 }
+ 	?>	
+			</div>
+			<?PHP include('footer.inc');?>
+		</div>
+	</body>
+</html>
