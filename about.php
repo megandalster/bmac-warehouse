4ccc561f46505d132f54356c8bab4277dd46d19b
martinda <martinda@whitman.edu>
2015-04-24 14:12:45 -0700

    Changed format of contributions search and shipment search

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?PHP
 /*
- * Copyright 2013 by Sawyer Bowman, Jim Garvey, Kevin Tabb, Nick Wetzel, and Allen
- * Tucker.  This program is part of Homerestore, which is free software.  It comes
+ * Copyright 2015 by Moustafa El Badry, Noah Jensen, Dylan Martin, Luis Munguia Orta,
+ * David Quennoz, and Allen Tucker.  This program is part of Homerestore, which is free software.  It comes
  * with absolutely no warranty.  You can redistribute and/or modify it under the
  * terms of the GNU Public License as published by the Free Software Foundation
  * (see <http://www.gnu.org/licenses/).
