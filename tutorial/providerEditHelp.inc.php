34d5454713b296b1e95488ce0045e8bfdb3c7f22
quennoda <quennoda@whitman.edu>
2015-04-25 14:19:39 -0700

    added provider edit help page

--- tutorial/providerEditHelp.inc.php
+++ tutorial/providerEditHelp.inc.php
@@ -29,79 +29,77 @@
 
 <p>
 	<strong>Editing and Adding a New Provider</strong>
-<p>Editing information in the volunteer database is usually done by the
-	House Manager. There is one exception: a volunteer can view and modify
-	his or her own information in the database (but nobody else's). So this
-	tutorial is mainly for the House Manager.
+<p>Note that editing information in the provider database can only be done by manager accounts.
 <p>
-	<strong>Step 1:</strong> First you need to <strong>search</strong> for
-	the person whose information you want to edit. <BR> <BR> <a
-		href="tutorial/screenshots/editpersonstep1.png" class="image"
-		title="editpersonstep1.png"
-		target="tutorial/screenshots/editpersonstep1.png">
+	<strong>Step 1:</strong> First you must navigate to the <strong>providers</strong> search page.
+	<BR> <BR> <a
+		href="tutorial/screenshots/editproviderstep1.png" class="image"
+		title="editproviderstep1.png"
+		target="tutorial/screenshots/editproviderstep1.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/editpersonstep1.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/editpersonstep1.png"
+		src="tutorial/screenshots/editproviderstep1.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/editproviderstep1.png"
+		border="1px" align="middle"> </a>
+</p>
+<p>
+	<strong>Step 2:</strong> To edit an existing provider use the search function and then click on
+	the provider's name. To add a new provider click on <strong>Add new provider</strong>.
+	You will now see a page with all of the provider's information, like this:
+	<BR> <BR> <a
+		href="tutorial/screenshots/editproviderstep2.png" class="image"
+		title="editproviderstep2.png"
+		target="tutorial/screenshots/editproviderstep2.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/editproviderstep2.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/editproviderstep2.png"
 		border="1px" align="middle"> </a>
 </p>
 <p>
 	To see more information on searching, <a
-		href="?helpPage=searchPeople.php">click here</a>.
+		href="?helpPage=bmac-warehouse/providerSearch.php">click here</a>.
+</p>
+<p>
+	<strong>Step 3:</strong> To change any of the provider's information,
+	just retype or reselect it. For instance, to change a provider's status click on the status
+	drop down menu and select their new status. <BR>&nbsp&nbsp REMEMBER: No fields marked by <font
+	color="#FF0000">*</font> can be left blank. If creating a new provider, status defaults to 
+	active and type to food.
 </p>
 
 <p>
-	<strong>Step 2:</strong> After finding that person, <strong> click on </strong>
-	his/her name. You will now see a page with all of the person's
-	information, like this:<BR> <BR> <a
-		href="tutorial/screenshots/editpersonstep2.png" class="image"
-		title="editpersonstep2.png"
-		target="tutorial/screenshots/editpersonstep2.png">
+	<strong>Step 4:</strong> When you finish making changes, select <strong>submit</strong>
+	at the bottom of the page. If you wish to delete a provider, check the box and click on <strong>delete</strong>:
+	<BR> <BR> <a
+		href="tutorial/screenshots/editproviderstep4.png" class="image"
+		title="editproviderstep4.png"
+		target="tutorial/screenshots/editproviderstep4.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/editpersonstep2.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/editpersonstep2.png"
-		border="1px" align="middle"> </a>
-</p>
-<p>
-	<strong>Step 3:</strong> To change any of the person's information,
-	just retype or reselect it. For instance, to change Availability,
-	select one of the unchecked (checked) boxes and it will become checked
-	(unchecked). <BR>&nbsp&nbsp REMEMBER: No fields marked by <font
-		color="#FF0000">*</font> can be left blank.
-</p>
-
-<p>
-	<strong>Step 4:</strong> When you finish making changes, select <strong>Submit</strong>
-	at the bottom of the page:<BR> <BR> <a
-		href="tutorial/screenshots/editpersonstep4.png" class="image"
-		title="editpersonstep4.png"
-		target="tutorial/screenshots/editpersonstep4.png">
-		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/editpersonstep4.png" rel="popover"
-		data-img="tutorial/screenshots/editpersonstep4.png" width="10%"
+		src="tutorial/screenshots/editproviderstep4.png" rel="popover"
+		data-img="tutorial/screenshots/editproviderstep4.png" width="10%"
 		border="1px" align="middle"> </a>
 </p>
 
 <p>
 	<B>Step 5:</B> If errors occur, <font color=#FF0000>red</font> warnings
 	will tell you what you need to correct, like this:<BR> <BR> <a
-		href="tutorial/screenshots/editpersonstep5.png" class="image"
-		title="editpersonstep5.png"
-		target="tutorial/screenshots/editpersonstep5.png"
+		href="tutorial/screenshots/editproviderstep5.png" class="image"
+		title="editproviderstep5.png"
+		target="tutorial/screenshots/editproviderstep5.png"
 		horizontalalign="center"> &nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/editpersonstep5.png" width="10%"
+		src="tutorial/screenshots/editproviderstep5.png" width="10%"
 		border="1px" rel="popover"
-		data-img="tutorial/screenshots/editpersonstep5.png" align="middle"> </a>
+		data-img="tutorial/screenshots/editproviderstep5.png" align="middle"> </a>
 	<BR>&nbsp&nbsp&nbsp *After you have make these corrections, repeat <B>Step
 		4</B>.
 <p>
 	<B>Step 6:</B> If you have no errors or omissions, you will see a page
 	telling you the edit was successful, like this:<BR> <BR> <a
-		href="tutorial/screenshots/editpersonstep6.png" class="image"
-		title="editpersonstep6.png"
-		target="tutorial/screenshots/editpersonstep6.png"
+		href="tutorial/screenshots/editproviderstep6.png" class="image"
+		title="editproviderstep6.png"
+		target="tutorial/screenshots/editproviderstep6.png"
 		horizontalalign="center"> &nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/editpersonstep6.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/editpersonstep6.png"
+		src="tutorial/screenshots/editproviderstep6.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/editproviderstep6.png"
 		border="1px" align="middle"> </a>
 <p>
 	<B>Step 7:</B> When you finish, you can return to any other function by
