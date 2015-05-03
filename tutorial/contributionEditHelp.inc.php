e2d46dfd581acdc52da5db23554ba0e51e9b9130
munguolm <munguolm@whitman.edu>
2015-05-02 21:33:35 -0700

    Added missing section to contributionEdit

--- tutorial/contributionEditHelp.inc.php
+++ tutorial/contributionEditHelp.inc.php
@@ -28,13 +28,13 @@
 </script>
 
 <p>
-	<strong>Editing and Adding a New Receipt</strong>
+	<strong>Adding, Editing, or Deleting a Receipt</strong>
 	</p>
 <p>This tutorial is compromised of two parts: Adding a new receipt into the database, and editing an existing receipt. 
 </p>
 <p><b>Adding a new receipt</b></p>
 <p>
-	<strong>Step 1:</strong>First, select the <b>Receipt Date box</b> and pick the date of the receipt by clicking on the calendar day. The date of the 
+	<strong>Step 1: </strong>First, select the <b>Receipt Date box</b> and pick the date of the receipt by clicking on the calendar day. The date of the 
 	receipt is set to the current date by default.
 	This will be the unique identifier for the receipt to be made. 
 	<BR> <BR> <a
@@ -61,7 +61,7 @@
 		
 </p>
 <p>
-	<strong>Step 2:</strong>If any, add the products that you received by filling in the appropiate boxes per individual product. If you are adding products, 
+	<strong>Step 2: </strong>If any, add the products that you received by filling in the appropiate boxes per individual product. If you are adding products, 
 	you must add the name of the <b>provider</b>, the amount received in <b>case lots</b>, and the <b>total weight</b> of the product. Optionally, you may also 
 	add the <b>individual</b> weight per case. 
 	
@@ -159,3 +159,91 @@
 		rel="popover" data-img="tutorial/screenshots/contributionEditHelp6-2.png"
 		border="1px" align="middle"> </a>
 <p>This means you're done!</p>
+
+<p><b>Editing an existing receipt</b></p>
+<p>Editing an existing receipt is very straightforward. You can modify the <b>Provider</b> box if it was misspelt, or pick a different <b>Receipt Date</b>. You can also change 
+the <b>Amount billed</b>, or the <b>Notes</b> about the receipt. 
+<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7.png" class="image"
+		title="contributionEditHelp7.png"
+		target="tutorial/screenshots/contributionEditHelp7.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7.png"
+		border="1px" align="middle"> </a></p>
+<p>You can also change the information of the products received, or add missing products. 
+<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-2.png" class="image"
+		title="contributionEditHelp6-2.png"
+		target="tutorial/screenshots/contributionEditHelp7-2.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-2.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-2.png"
+		border="1px" align="middle"> </a></p>
+<p>To delete existing products, simply clear out the row of the product you wish to delete
+from the receipt.
+<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-3.png" class="image"
+		title="contributionEditHelp7-3.png"
+		target="tutorial/screenshots/contributionEditHelp7-3.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-3.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-3.png"
+		border="1px" align="middle"> </a>
+</p>
+<p>
+	When you're done, hit <b>Submit</b>. 
+	
+	<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-5.png" class="image"
+		title="contributionEditHelp5.png"
+		target="tutorial/screenshots/contributionEditHelp7-5.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-5.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-5.png"
+		border="1px" align="middle"> </a>
+		
+</p>
+<p>
+
+	If you missed a required field, a warning will pop up. In this case, the <b>provider name</b> was left blank. 
+	
+	<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-4.png" class="image"
+		title="contributionEditHelp7-4.png"
+		target="tutorial/screenshots/contributionEditHelp7-4.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-4.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-4.png"
+		border="1px" align="middle"> </a>
+		
+</p>
+<p>If you didn't make any mistakes, you will be shown a page like this:
+<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-6.png" class="image"
+		title="contributionEditHelp7-6.png"
+		target="tutorial/screenshots/contributionEditHelp7-6.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-6.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-6.png"
+		border="1px" align="middle"> </a>
+<p>This means you're done!</p>
+<p><b>Deleting an existing receipt</b></p>
+<p>To delete an existing receipt, <b>check the box shown below</b> and hit <b>delete</b>:
+<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-7.png" class="image"
+		title="contributionEditHelp7-7.png"
+		target="tutorial/screenshots/contributionEditHelp7-7.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-7.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-7.png"
+		border="1px" align="middle"> </a>
+<p>A confirmation that you deleted the receipt will be shown.
+<BR> <BR> <a
+		href="tutorial/screenshots/contributionEditHelp7-8.png" class="image"
+		title="contributionEditHelp7-8.png"
+		target="tutorial/screenshots/contributionEditHelp7-8.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionEditHelp7-8.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionEditHelp7-8.png"
+		border="1px" align="middle"> </a>
