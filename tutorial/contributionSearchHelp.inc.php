5a73dd3a8068801260c519b59a8b6190cc8fc0a5
munguolm <munguolm@whitman.edu>
2015-05-02 21:04:31 -0700

    Added pictures to contributionEditHelp

--- tutorial/contributionSearchHelp.inc.php
+++ tutorial/contributionSearchHelp.inc.php
@@ -34,7 +34,7 @@
 <p>
 Here you can search for the receipts of contributions received. You can search by the date range in which receipts were made, by the name of the provider, and by the name of the
 received item. You can view the products received per contribution, their amount in case lots, and weight of each product. It also includes total weight of contribution and the amount
-billed for the receipt. You can also select individual receipts to edit them.
+billed for the receipt. You can also select individual receipts to edit them, or add a new receipt.
 </p>
 <p>
 	<B>Step 1:</B> On the navigation bar at the top of the page, click on the date range boxes and select the dates you want to search on by clicking on the day box:<BR> <BR><a
@@ -75,6 +75,16 @@ billed for the receipt. You can also select individual receipts to edit them.
 		target="tutorial/screenshots/contributionSearchHelp3.png">
 		&nbsp&nbsp&nbsp&nbsp<img
 		src="tutorial/screenshots/contributionSearchHelp3.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/contributionSearchHelp2.png"
+		rel="popover" data-img="tutorial/screenshots/contributionSearchHelp3.png"
+		border="1px" align="middle"> </a>
+</p>
+<p>
+	<B>Step 4:</B> Click on <b>Add new receipt</b> to add a new receipt into the database. <b><a href="help.php?helpPage=bmac-warehouse/contributionEdit.php">Adding a new a receipt</a></b> <BR> <BR><a
+		href="tutorial/screenshots/contributionSearchHelp4.png" class="image"
+		title="contributionSearchHelp4.png"
+		target="tutorial/screenshots/contributionSearchHelp4.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/contributionSearchHelp4.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/contributionSearchHelp4.png"
 		border="1px" align="middle"> </a>
 </p>
