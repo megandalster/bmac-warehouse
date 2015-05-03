2b31684421e2257cfbf6d2a46fee2f2e67241fdb
munguolm <munguolm@whitman.edu>
2015-05-02 18:18:49 -0700

    Pictures from inventoryHelp

--- tutorial/inventoryHelp.inc.php
+++ tutorial/inventoryHelp.inc.php
@@ -31,80 +31,92 @@
 </p>
 <p>
 	<B>Step 1:</B> On the navigation bar at the top of the page, find <B>Work on inventory for all active products with names that begin with:</B>
-	and type in the search box the products you want to review, like this:<BR> <BR> <a
-		href="tutorial/screenshots/addpersonhelpstep1.png" class="image"
-		title="addpersonhelpstep1.png" horizontalalign="center"
-		target="tutorial/screenshots/addpersonhelpstep1.png">
+	and type in the search box the initial letters of the products you want to review. Then, press enter or update:<BR> <BR> <a
+		href="tutorial/screenshots/inventoryHelp1.png" class="image"
+		title="inventoryHelp1.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp1.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/addpersonhelpstep1.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/addpersonhelpstep1.png"
+		src="tutorial/screenshots/inventoryHelp1.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp1.png"
 		border="1px" align="center"> </a>
 </p>
 <p>
-	<B>Step 2:</B> You should now see a long list of all products that begin with whatever you typed
-		in the search box:<BR> <BR> <a
-		href="tutorial/screenshots/addpersonhelpstep2.png" class="image"
-		title="addpersonhelpstep2.png"
-		target="tutorial/screenshots/addpersonhelpstep2.png">
+	<B>Step 2:</B> You should now see a long list of all products that match your search parameters:<BR> <BR> <a
+		href="tutorial/screenshots/inventoryHelp2.png" class="image"
+		title="inventoryHelp2.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp2.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/addpersonhelpstep2.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/addpersonhelpstep2.png"
-		border="1px" align="middle"> </a> <BR> You can scroll down to see the full list. <br>(NOTE:&nbsp All fields marked
+		src="tutorial/screenshots/inventoryHelp2.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp2.png"
+		border="1px" align="center"> </a><BR> You can scroll down to see the full list. <br>(NOTE:&nbsp All fields marked
 		by <font color="#FF0000">*</font> are required before a person can be
 	added to the database.)
 </p>
 
 <p>
 	<B>Step 3:</B> There will be two boxes at the right of each product information, where you can edit their <B>Current Stock in case lots</B> and
-	their <B>Current Stock in total weight</B> by typing them in the box.<BR> <BR> <a
-		href="tutorial/screenshots/addpersonhelpstep3.png" class="image"
-		title="addpersonhelpstep3.png"
-		target="tutorial/screenshots/addpersonhelpstep3.png">
+	their <B>Current Stock in total weight</B> by typing them in the box and hitting update.<BR> <BR> <b>1.</b><a
+		href="tutorial/screenshots/inventoryHelp3.png" class="image"
+		title="inventoryHelp3.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp3.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/addpersonhelpstep3.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/addpersonhelpstep3.png"
-		border="1px" align="middle"> </a>
+		src="tutorial/screenshots/inventoryHelp3.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp3.png"
+		border="1px" align="center"> </a>
+		<BR> <BR> <b>2.</b><a
+		href="tutorial/screenshots/inventoryHelp3-2.png" class="image"
+		title="inventoryHelp3-2.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp3-2.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/inventoryHelp3-2.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp3-2.png"
+		border="1px" align="center"> </a>
 
 </p>
 <p>
-	<B>Step 4:</B> If you leave either box blank, it will be automatically computed given the last inventory check of the product, plus its recent shipments and receipts, like so:<BR> <BR> <a
-		href="tutorial/screenshots/addpersonhelpstep3.png" class="image"
-		title="addpersonhelpstep3.png"
-		target="tutorial/screenshots/addpersonhelpstep3.png">
+	<B>Step 4:</B> If you leave either box blank, it will be automatically computed given the last inventory check of the product, plus its recent shipments and receipts, like so:<BR> <BR> <b>1.</b><a
+		href="tutorial/screenshots/inventoryHelp4.png" class="image"
+		title="inventoryHelp4.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp4.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/addpersonhelpstep3.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/addpersonhelpstep3.png"
-		border="1px" align="middle"> </a>
+		src="tutorial/screenshots/inventoryHelp4.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp4.png"
+		border="1px" align="center"> </a>
+		<BR> <BR> <b>2.</b><a
+		href="tutorial/screenshots/inventoryHelp4-2.png" class="image"
+		title="inventoryHelp4-2.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp4-2.png">
+		&nbsp&nbsp&nbsp&nbsp<img
+		src="tutorial/screenshots/inventoryHelp4-2.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp4-2.png"
+		border="1px" align="center"> </a>
+
 </p>
 		
 		
 		
 		
 <p>
-	<B>Step 4:</B> If you make a mistake or omit a required field, <font
-		color=#FF0000>red</font> warnings will tell you what you need to
-	correct, like this:<BR> <BR> <a
-		href="tutorial/screenshots/addpersonhelpstep4.png" class="image"
-		title="addpersonhelpstep4.png" horizontalalign="center"
-		target="tutorial/screenshots/addpersonhelpstep4.png">
+	<B>Step 5:</B> If you make a mistake or omit a required field, check the <b>Undo</b> box and hit update on the product you wish to revert changes:<BR> <BR> <a
+		href="tutorial/screenshots/inventoryHelp5.png" class="image"
+		title="inventoryHelp5.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp5.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/addpersonhelpstep4.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/addpersonhelpstep4.png"
-		border="1px" align="middle"> </a> <br>After you make these
-		corrections, repeat <B>Step 3</B>.
-</p>
-<p>
-	<B>Step 5:</B> If you have no errors or omissions, you will see a page
-	that looks like this:<BR> <BR> <a
-		href="tutorial/screenshots/addpersonhelpstep5.png" class="image"
-		title="addpersonhelpstep5.png" horizontalalign="center"
-		target="tutorial/screenshots/addpersonhelpstep5.png">
+		src="tutorial/screenshots/inventoryHelp5.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp5.png"
+		border="1px" align="center"> </a>
+		<BR> <BR> <b>2.</b><a
+		href="tutorial/screenshots/inventoryHelp5-2.png" class="image"
+		title="inventoryHelp5-2.png" horizontalalign="center"
+		target="tutorial/screenshots/inventoryHelp5-2.png">
 		&nbsp&nbsp&nbsp&nbsp<img
-		src="tutorial/screenshots/addpersonhelpstep5.png" width="10%"
-		rel="popover" data-img="tutorial/screenshots/addpersonhelpstep5.png"
-		border="1px" align="middle"> </a>
+		src="tutorial/screenshots/inventoryHelp5-2.png" width="10%"
+		rel="popover" data-img="tutorial/screenshots/inventoryHelp5-2.png"
+		border="1px" align="center"> </a>
+
 </p>
+
 <p>
 	<B>Step 6:</B> When you finish, you can return to any other function by
-	selecting it on the navigation bar.
+	selecting it on the navigation bar. 
 </p>
